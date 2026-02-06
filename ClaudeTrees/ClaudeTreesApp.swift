9fba5ce83eb5a5bf44ee73fe8ce21bb5279b97cb
Patryk Radziszewski <patryk@chef.ski>
2026-02-06 14:56:36 +0000

    Initial Commit

---
+++ ClaudeTrees/ClaudeTreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeTreesApp.swift
+//  ClaudeTrees
+//
+//  Created by Patryk Radziszewski on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeTreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeTreesTests/ClaudeTreesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeTreesTests.swift
+//  ClaudeTreesTests
+//
+//  Created by Patryk Radziszewski on 06/02/2026.
+//
+
+import Testing
+@testable import ClaudeTrees
+
+struct ClaudeTreesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
